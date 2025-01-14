09f1de83a17bc2a8b82748c5e009b16d94875eaa
shahidulllah <mdshahidsumon177@gmail.com>
2025-01-14 21:07:06 +0600

    toaster

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -4,42 +4,53 @@ import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { verifyToken } from "../utils/verifyToken";
 import { setUser } from "../redux/features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const SignIn = () => {
-    const dispatch = useAppDispatch()
-    const { register, handleSubmit} = useForm({
-        defaultValues: {
-            userId: 'A-0001',
-            password: 'admin1234'
-        }
-    })
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: "A-0001",
+      password: "admin1234",
+    },
+  });
 
-    const [login, {error}] = useLoginMutation()
+  const [login, { error }] = useLoginMutation();
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const onSubmit = async (data: any) => {
-        console.log(data);
-        const userInfo = {
-            id: data.userId,
-            password: data.password,
-        }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onSubmit = async (data: any) => {
+    const toastId = toast.loading("loading in");
+    try {
+      const userInfo = {
+        id: data.userId,
+        password: data.password,
+      };
 
-        const res = await login (userInfo).unwrap();
-        const user = verifyToken(res.data.accessToken)
+      const res = await login(userInfo).unwrap();
+      const user = verifyToken(res.data.accessToken);
 
-        dispatch(setUser({
-            user: user, 
-            token: res.data.accessToken
-        }))
+      dispatch(
+        setUser({
+          user: user,
+          token: res.data.accessToken,
+        })
+      );
+      toast.success("login success", { id: toastId, duration: 2000 });
+      navigate(`/${user.role}/dashboard`);
+    } catch (err) {
+      if (err) {
+        toast.error("Something went wrong", { id: toastId, duration: 2000 });
+      }
     }
+  };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="id">ID: </label>
         <input type="text" id="id" {...register("userId")} />
-        {
-          error && <p>Id is required</p>
-        }
+        {error && <p>Id is required</p>}
       </div>
       <div>
         <label htmlFor="password">Password: </label>
