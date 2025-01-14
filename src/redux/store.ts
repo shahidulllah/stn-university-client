f74ffd435a25e049a8b194dd8f5174c9c59998bb
shahidulllah <mdshahidsumon177@gmail.com>
2025-01-14 14:05:52 +0600

    wrap persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,31 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
-import storage from 'redux-persist/lib/storage';
-import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
 import { baseApi } from "./api/baseApi";
 
-
 const persistConfig = {
-  key: 'auth',
-  storage
-  
-}
+  key: "auth",
+  storage,
+};
 const persistedAuthReducer = persistReducer(persistConfig, authReducer);
-
 
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
     auth: persistedAuthReducer,
   },
-  middleware: (getDefaultMiddlewares) => getDefaultMiddlewares({
-    serializableCheck: {
-      ignoreActions: [FLUSH, REHYDRATE, PAUSE, PURGE, PERSIST, REGISTER]
-    }
-  }).concat(baseApi.middleware)
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PURGE, PERSIST, REGISTER],
+      },
+    }).concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-export const persistor = persistStore (store)
+export const persistor = persistStore(store);
