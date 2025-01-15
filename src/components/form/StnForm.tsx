39df64633776dc8d97b6834e04cfadc0a7f6034a
shahidulllah <mdshahidsumon177@gmail.com>
2025-01-15 23:26:04 +0600

    working on stn form

--- src/components/form/StnForm.tsx
+++ src/components/form/StnForm.tsx
@@ -1,8 +1,35 @@
-import { useForm } from "react-hook-form";
+import { ReactNode } from "react";
+import {
+  FieldValues,
+  FormProvider,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
 
-const StnForm = ({ onSubmit }) => {
-  const { handleSubmit } = useForm();
-  return <form onSubmit={handleSubmit(onSubmit)}>This is form</form>;
+type TFormConfig = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  defaultValues?: Record<string, any>;
+};
+
+type TFormProps = {
+  onSubmit: SubmitHandler<FieldValues>;
+  children: ReactNode;
+} & TFormConfig;
+
+const StnForm = ({ onSubmit, children, defaultValues }: TFormProps) => {
+  const formConfig: TFormConfig = {};
+
+  if (defaultValues) {
+    formConfig["defaultValues"] = defaultValues;
+  }
+
+  const methods = useForm(formConfig);
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>{children}</form>
+    </FormProvider>
+  );
 };
 
 export default StnForm;
