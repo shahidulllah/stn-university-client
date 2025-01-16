95ee3f91e3828aa3fa2294cc73d3a7fc98528cbf
shahidulllah <mdshahidsumon177@gmail.com>
2025-01-16 18:54:08 +0600

    working on base api

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -8,6 +8,7 @@ import {
 } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 import { logout, setUser } from "../features/auth/authSlice";
+import { toast } from "sonner";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
@@ -30,6 +31,10 @@ const baseQureyWithRefreshToken: BaseQueryFn<
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
 > = async (args, api, extraOptions): Promise<any> => {
   let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 404){
+    toast.error("User not found")
+  }
 
   if (result?.error?.status === 401) {
     //send Refresh token
