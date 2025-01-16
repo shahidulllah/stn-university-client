4b9f9d06f8eba9ad72c9c0cdbf5843f033cd3715
shahidulllah <mdshahidsumon177@gmail.com>
2025-01-16 19:04:34 +0600

    create Academic department component

---
+++ src/pages/admin/academicManagement/AcademicDepartment.tsx
@@ -0,0 +1,10 @@
+
+const AcademicDepartment = () => {
+    return (
+        <div>
+              <h1> This is AcademicDepartment component </h1>
+        </div>
+    );
+};
+
+export default AcademicDepartment;
