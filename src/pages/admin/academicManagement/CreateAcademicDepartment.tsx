1314f57f0e43a679b8bbcdc54696e50baa264a87
shahidulllah <mdshahidsumon177@gmail.com>
2025-01-16 19:07:46 +0600

    CreateAcademicDepartment

---
+++ src/pages/admin/academicManagement/CreateAcademicDepartment.tsx
@@ -0,0 +1,11 @@
+
+
+const CreateAcademicDepartment = () => {
+    return (
+        <div>
+              <h1> This is CreateAcademicDepartment component </h1>
+        </div>
+    );
+};
+
+export default CreateAcademicDepartment;
