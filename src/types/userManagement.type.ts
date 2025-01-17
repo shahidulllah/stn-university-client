56a3c4e19405a438b55ecea4081e2610526a59c5
shahidulllah <mdshahidsumon177@gmail.com>
2025-01-17 22:05:43 +0600

    User type

---
+++ src/types/userManagement.type.ts
@@ -0,0 +1,12 @@
+export type TUser = {
+  _id: string;
+  id: string;
+  email: string;
+  needsPasswordChange: boolean;
+  role: string;
+  status: string;
+  isDeleted: boolean;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+};
